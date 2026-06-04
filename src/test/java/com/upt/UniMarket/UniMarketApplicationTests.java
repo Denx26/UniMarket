84f26935b8aecce07428b3856de34520ded84731
Denx26 <denisiosif26@gmail.com>
2026-06-04 19:54:16 +0300

    Initial commit

---
+++ src/test/java/com/upt/UniMarket/UniMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upt.UniMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
