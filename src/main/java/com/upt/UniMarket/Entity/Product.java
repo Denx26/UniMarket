9618e70631371ff8648380f2b1add0b1cbc4a44e
ledtylenol <ledtylenol@duck.com>
2026-06-06 14:23:30 +0300

    produs entity

---
+++ src/main/java/com/upt/UniMarket/Entity/Product.java
@@ -0,0 +1,32 @@
+package com.upt.UniMarket.Entity;
+
+import java.util.Optional;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long pid;
+
+	private String nume;
+	private Float pret;
+	private String descriere;
+	private Boolean negociabil;
+	@Column(nullable = true)
+	private Float pretMin;
+
+	public boolean pretValid(float pret) {
+		return pretMin != null && pret > pretMin || pret > this.pret;
+	}
+
+	public void setNegociabil(boolean negociabil) {
+		this.negociabil = negociabil;
+	}
+
+	public boolean getNegeciabil() {
+		return negociabil;
+	}
+}
