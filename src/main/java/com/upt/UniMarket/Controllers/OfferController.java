a218aedc92bdb23ce4f40b9b042557cef12e37bc
Alexandru-T <tibirnaalexandru@gmail.com>
2026-06-09 14:16:10 +0300

    offer

---
+++ src/main/java/com/upt/UniMarket/Controllers/OfferController.java
@@ -0,0 +1,38 @@
+package com.upt.UniMarket.Controllers;
+
+
+import com.upt.UniMarket.Dto.OfferRequest;
+import com.upt.UniMarket.Entity.Offer;
+import com.upt.UniMarket.Services.OfferService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/offers")
+public class OfferController {
+
+    @Autowired
+    private OfferService offerService;
+
+    @PostMapping
+    public ResponseEntity<?> createOffer(@RequestBody OfferRequest request){
+        try{
+            Offer savedOffer = offerService.createOffer(request);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedOffer);
+        } catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/{ooferId}/approve")
+    public ResponseEntity<?> approveOffer(@PathVariable Long offerId) {
+        try {
+            offerService.approveOffer(offerId);
+            return ResponseEntity.ok("Offer approved successfully, product has been sold");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+}
