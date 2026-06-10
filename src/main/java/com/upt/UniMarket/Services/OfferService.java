362cd198d43762d3f265629e25f17ffa7cd39e9a
Alexandru-T <tibirnaalexandru@gmail.com>
2026-06-11 01:10:35 +0300

    adaugat partea de history la OfferService

--- src/main/java/com/upt/UniMarket/Services/OfferService.java
+++ src/main/java/com/upt/UniMarket/Services/OfferService.java
@@ -5,6 +5,8 @@ import com.upt.UniMarket.Entity.Offer;
 import com.upt.UniMarket.Entity.Product;
 import com.upt.UniMarket.Repositories.OfferRepository;
 import com.upt.UniMarket.Repositories.ProductRepository;
+import com.upt.UniMarket.Repositories.TransactionRepository;
+import com.upt.UniMarket.Repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,11 @@ public class OfferService {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private TransactionRepository transactionRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     public Offer createOffer(OfferRequest request) throws Exception {
         Product product = productRepository.findById(request.getProductId()).orElseThrow(() -> new Exception("Product not found"));
@@ -42,12 +49,30 @@ public class OfferService {
 
     @Transactional
     public void approveOffer(Long offerId) throws Exception {
-        Offer offer = offerRepository.findById(offerId).orElseThrow(()->new Exception("Offer not found"));
+        Offer offer = offerRepository.findById(offerId)
+                .orElseThrow(() -> new Exception("Offer not found"));
 
-        // partea cu sales history
+        Product product = productRepository.findById(offer.getProductId())
+                .orElseThrow(() -> new Exception("Product not found"));
+
+        String buyerEmail = userRepository.findById(offer.getBuyerId())
+                .map(user -> user.getEmail())
+                .orElse("unknown_buyer@email.com");
+
+        String sellerEmail = userRepository.findById(product.getVanzatorId())
+                .map(user -> user.getEmail())
+                .orElse("unknown_seller@email.com");
+
+        com.upt.UniMarket.Entity.Transaction transaction = new com.upt.UniMarket.Entity.Transaction(
+                product.getNume(),
+                offer.getProposedPrice().floatValue(),
+                buyerEmail,
+                sellerEmail
+        );
+
+        transactionRepository.save(transaction);
 
         offerRepository.deleteByProductId(offer.getProductId());
-
         productRepository.deleteById(offer.getProductId());
     }
 
